404114224b8326b7dfae97ad894201779636b1cc
Dan Piponi <dpiponi@gmail.com>
2015-10-14 21:20:03 -0700

    Very small

--- PDFBorder.swift
+++ PDFBorder.swift
@@ -46,6 +46,39 @@ func makeThumbnail(pdfFile: NSURL) -> UIImage {
     return thumbnailImage
 }
 
+func visitBorder
+for i in 0..<iwidth {
+    let index : Int = 4*i
+    let alpha : CGFloat = CGFloat(bdat[index+3])/255.0
+    tr += alpha*CGFloat(bdat[index])/CGFloat(255)+(1-alpha)
+    tg += alpha*CGFloat(bdat[index+1])/CGFloat(255)+(1-alpha)
+    tb += alpha*CGFloat(bdat[index+2])/CGFloat(255)+(1-alpha)
+    count += 1
+    
+    let index2 : Int = (iheight-1)*bytesPerRow+4*i
+    let alpha2 : CGFloat = CGFloat(bdat[index2+3])/255.0
+    tr += alpha2*CGFloat(bdat[index2])/CGFloat(255)+(1-alpha2)
+    tg += alpha2*CGFloat(bdat[index2+1])/CGFloat(255)+(1-alpha2)
+    tb += alpha2*CGFloat(bdat[index2+2])/CGFloat(255)+(1-alpha2)
+    count += 1
+}
+for i in 0..<iheight {
+    let index : Int = i*bytesPerRow
+    let alpha : CGFloat = CGFloat(bdat[index+3])/255.0
+    tr += alpha*CGFloat(bdat[index])/CGFloat(255)+(1-alpha)
+    tg += alpha*CGFloat(bdat[index+1])/CGFloat(255)+(1-alpha)
+    tb += alpha*CGFloat(bdat[index+2])/CGFloat(255)+(1-alpha)
+    count += 1
+    
+    let index2 : Int = (iwidth-1)*bytesPerPixel+i*bytesPerRow
+    let alpha2 : CGFloat = CGFloat(bdat[index2+3])/255.0
+    tr += alpha2*CGFloat(bdat[index2])/CGFloat(255)+(1-alpha2)
+    tg += alpha2*CGFloat(bdat[index2+1])/CGFloat(255)+(1-alpha2)
+    tb += alpha2*CGFloat(bdat[index2+2])/CGFloat(255)+(1-alpha2)
+    count += 1
+}
+
+
 // Pick colour for border around slide.
 func PDFBorder(document: CGPDFDocument, pageNumber: Int) -> UIColor {
     let downScale : Int = 4
@@ -106,13 +139,12 @@ func PDFBorder(document: CGPDFDocument, pageNumber: Int) -> UIColor {
         tg += alpha*CGFloat(bdat[index+1])/CGFloat(255)+(1-alpha)
         tb += alpha*CGFloat(bdat[index+2])/CGFloat(255)+(1-alpha)
         count += 1
-    }
-    for i in 0..<iwidth {
-        let index : Int = (iheight-1)*bytesPerRow+4*i
-        let alpha : CGFloat = CGFloat(bdat[index+3])/255.0
-        tr += alpha*CGFloat(bdat[index])/CGFloat(255)+(1-alpha)
-        tg += alpha*CGFloat(bdat[index+1])/CGFloat(255)+(1-alpha)
-        tb += alpha*CGFloat(bdat[index+2])/CGFloat(255)+(1-alpha)
+
+        let index2 : Int = (iheight-1)*bytesPerRow+4*i
+        let alpha2 : CGFloat = CGFloat(bdat[index2+3])/255.0
+        tr += alpha2*CGFloat(bdat[index2])/CGFloat(255)+(1-alpha2)
+        tg += alpha2*CGFloat(bdat[index2+1])/CGFloat(255)+(1-alpha2)
+        tb += alpha2*CGFloat(bdat[index2+2])/CGFloat(255)+(1-alpha2)
         count += 1
     }
     for i in 0..<iheight {
@@ -122,13 +154,12 @@ func PDFBorder(document: CGPDFDocument, pageNumber: Int) -> UIColor {
         tg += alpha*CGFloat(bdat[index+1])/CGFloat(255)+(1-alpha)
         tb += alpha*CGFloat(bdat[index+2])/CGFloat(255)+(1-alpha)
         count += 1
-    }
-    for i in 0..<iheight {
-        let index : Int = (iwidth-1)*bytesPerPixel+i*bytesPerRow
-        let alpha : CGFloat = CGFloat(bdat[index+3])/255.0
-        tr += alpha*CGFloat(bdat[index])/CGFloat(255)+(1-alpha)
-        tg += alpha*CGFloat(bdat[index+1])/CGFloat(255)+(1-alpha)
-        tb += alpha*CGFloat(bdat[index+2])/CGFloat(255)+(1-alpha)
+
+        let index2 : Int = (iwidth-1)*bytesPerPixel+i*bytesPerRow
+        let alpha2 : CGFloat = CGFloat(bdat[index2+3])/255.0
+        tr += alpha2*CGFloat(bdat[index2])/CGFloat(255)+(1-alpha2)
+        tg += alpha2*CGFloat(bdat[index2+1])/CGFloat(255)+(1-alpha2)
+        tb += alpha2*CGFloat(bdat[index2+2])/CGFloat(255)+(1-alpha2)
         count += 1
     }
     tr /= CGFloat(count)
